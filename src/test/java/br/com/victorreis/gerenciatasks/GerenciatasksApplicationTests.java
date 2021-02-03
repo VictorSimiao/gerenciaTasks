255107ba83f9342350fa0e5eb7c0aa7fade7b28a
Victor Simiao <victor.sreis@hotmail.com>
2021-02-03 17:07:34 -0300

    Iniciando o projeto

---
+++ src/test/java/br/com/victorreis/gerenciatasks/GerenciatasksApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.victorreis.gerenciatasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciatasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
