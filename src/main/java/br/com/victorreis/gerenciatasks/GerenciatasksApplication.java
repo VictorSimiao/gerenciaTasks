d6d78f3cd4e27431ffceecc39724fe82ae0b0d80
Victor Simiao <victor.sreis@hotmail.com>
2021-02-03 21:32:33 -0300

    Expondo o ID das tarefas via web services

--- src/main/java/br/com/victorreis/gerenciatasks/GerenciatasksApplication.java
+++ src/main/java/br/com/victorreis/gerenciatasks/GerenciatasksApplication.java
@@ -2,12 +2,20 @@ package br.com.victorreis.gerenciatasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+import br.com.victorreis.gerenciatasks.domain.task.Task;
 
 @SpringBootApplication
-public class GerenciatasksApplication {
+public class GerenciatasksApplication  implements RepositoryRestConfigurer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(GerenciatasksApplication.class, args);
+	}
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+		config.exposeIdsFor(Task.class);
 	}
 
 }
