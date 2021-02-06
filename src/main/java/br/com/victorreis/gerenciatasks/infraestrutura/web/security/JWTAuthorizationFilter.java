e29070e3e0596efd051f7950f2d275760a0c074e
Victor Simiao <victor.sreis@hotmail.com>
2021-02-05 23:19:34 -0300

    Finalizando a criação o AuthorizationFilter

--- src/main/java/br/com/victorreis/gerenciatasks/infraestrutura/web/security/JWTAuthorizationFilter.java
+++ src/main/java/br/com/victorreis/gerenciatasks/infraestrutura/web/security/JWTAuthorizationFilter.java
@@ -1,5 +1,52 @@
 package br.com.victorreis.gerenciatasks.infraestrutura.web.security;
 
-public class JWTAuthorizationFilter {
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import io.jsonwebtoken.Jwts;
+
+public class JWTAuthorizationFilter extends BasicAuthenticationFilter{
+
+	public JWTAuthorizationFilter(AuthenticationManager authenticationManager) {
+		super(authenticationManager);
+		
+	}
+	
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		//Pegando a informção do header
+		String token = request.getHeader(SecurityConstants.AUTHORIZATION_HEADER);
+		
+		if (token != null && token.startsWith(SecurityConstants.TOKEN_PREFIX)) {
+			UsernamePasswordAuthenticationToken authentication = getAuthentcation(token);
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+		}
+		
+		chain.doFilter(request, response);
+	}
+	
+	//Extraindo informações do token
+	private UsernamePasswordAuthenticationToken getAuthentcation(String token) {
+		String username = Jwts.parser().setSigningKey(SecurityConstants.SECRET_KEY) //decodificando o token
+			.parseClaimsJws(token.replace(SecurityConstants.TOKEN_PREFIX, "")) //tirando o Bearer : só sobra o token
+			.getBody().getSubject(); //pegando o username
+		
+		if (username != null) {
+			return new UsernamePasswordAuthenticationToken(username, null, AuthorityUtils.NO_AUTHORITIES);
+		}
+		
+		return null;
+	}
 
 }
