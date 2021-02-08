0756b6af7cfe98eca8a7e41ecab7f5ac938b13fc
Victor Simiao <victor.sreis@hotmail.com>
2021-02-08 14:19:47 -0300

    Retornando apenas as tarefas de um usuário específico

--- src/main/java/br/com/victorreis/gerenciatasks/domain/task/TaskRepository.java
+++ src/main/java/br/com/victorreis/gerenciatasks/domain/task/TaskRepository.java
@@ -1,7 +1,20 @@
 package br.com.victorreis.gerenciatasks.domain.task;
 
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TaskRepository extends JpaRepository<Task, Integer> {
 	Task findByDescricao(String descricao);
+	
+	@Override
+	@Query("SELECT t FROM Task t WHERE t.appUser.username = ?#{principal}")
+	Page<Task> findAll(Pageable pageable);
+	
+	@Override
+	@Query("SELECT t FROM Task t WHERE t.id = ?1 AND t.appUser.username = ?#{principal}")
+	Optional<Task> findById(Integer id);
 }
